a8ab1335c1c9d6fb7d5e273e5802e14983a40cd0
sltm-14 <ie693242@iteso.mx>
2020-01-27 12:51:15 -0600

    Se borraron los archivos de python para hacer un nuevo repositorio

--- 1 - Python101/Variables/string_methods.py
+++ 1 - Python101/Variables/string_methods.py
@@ -1,32 +1,32 @@
 
 course = "      Python programming     "
 
-# Prints string to upper cases
+""" Prints string to upper cases """
 print(course.upper())  # PYTHON PROGRAMMING    .
 
-# Prints string to lower cases
+""" Prints string to lower cases """
 print(course.lower())  # python programming    .
 
-# Prints the first letter of every word to upper case
+""" Prints the first letter of every word to upper case """
 print(course.title())  # Python Programming    .
 
-# Gets rid of the blanck spaces at the beggining or the end of the string.
+""" Gets rid of the blanck spaces at the beggining or the end of the string. """
 print(course.strip())  # Python programming.
 
-# If not specified, gets rid of the blank spaces at the end of the string...
+""" If not specified, gets rid of the blank spaces at the end of the string... """
 print(course.rstrip())  # Python programming.
 
-# If specified, gets rid of the spesified string if the oiginal string ends
-# with some spesified parameters and stops until it finds a character that is not a parameter
+""" If specified, gets rid of the spesified string if the oiginal string ends
+    with some spesified parameters and stops until it finds a character that is not a parameter """
 print(course.rstrip('ming '))  # Python programming.
 
-# Characters before 'pro'
+""" Characters before 'pro' """
 print(course.find("pro"))           # 13
 
-# Characters before 'pro'
+""" Characters before 'pro' """
 print(course.find("Pro"))           # -1 ('Pro' was not in the string)
 
-# Replaces every 'o' with '-'
+""" Replaces every 'o' with '-' """
 print(course.replace("o", "-"))     # Pyth-n pr-gramming   .
 
 print("programming" in course)      # True
